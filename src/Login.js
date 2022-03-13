30ea2b17576d87163b0a201fb163cc665da92782
Treadaa <87612934+Treadaa@users.noreply.github.com>
2022-03-13 22:07:23 +0530

    init

---
+++ src/Login.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import './Login.css'
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase";
+
+function Login() {
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
+    }
+
+    const register = e => {
+        e.preventDefault();
+
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // it successfully created a new user with email and password
+                if (auth) {
+                    history.push('/')
+                }
+            })
+            .catch(error => alert(error.message))
+    }
+
+    return (
+        <div className='login'>
+            <Link to='/'>
+                <img
+                    className="login__logo"
+                    src='https://imgs.search.brave.com/h82xhaCjajRh1jCYm1ZoW407nrOs_dSWv_3rA9fuKzA/rs:fit:512:512:1/g:ce/aHR0cHM6Ly9zZWVr/bG9nby5uZXQvd3At/Y29udGVudC91cGxv/YWRzLzIwMTcvMDMv/YW1hem9uLWFsZXhh/LWxvZ28ucG5n' 
+                />
+            </Link>
+
+            <div className='login__container'>
+                <h1>Sign-in</h1>
+
+                <form>
+                    <h5>E-mail</h5>
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+
+                    <h5>Password</h5>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+
+                    <button type='submit' onClick={signIn} className='login__signInButton'>Sign In</button>
+                </form>
+
+                <p>
+                    By signing-in you agree to the EON Conditions of Use & Sale. Please
+                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+
+                <button onClick={register} className='login__registerButton'>Create your  Account</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
